6a6855aa20451fb829af7d169bd28d902c2b2396
Mareme31 <m.t55@zig.univ.sn>
2023-11-11 12:22:32 +0000

    Mise a jour de la classe Service et ajout de son repository

--- BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/Service.java
+++ BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/Service.java
@@ -1,5 +1,75 @@
 package com.groupe5.domaine;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Service {
     
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable=false)
+	private Long idService;
+
+    @Column(nullable=false, unique=true)
+	private String nom;
+
+    @Column(nullable = false)
+    private String description;
+/*Utilisation de ManyToMany pour effectuer la jointure de Service et Prestataire */
+    @ManyToMany(targetEntity = Prestataire.class)
+    @JoinTable(name = "ServiceOfferts",joinColumns = @JoinColumn(name = "idService"), 
+                inverseJoinColumns = @JoinColumn(name = "idPrestataire")
+                )
+    private List<Prestataire> prestataires = new ArrayList<>();
+
+     /* --------------les Constructeurs--------------- */
+    public Service() {}
+
+    public Service(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    /* --------------les getters et les setters--------------- */
+
+    public Long getId() {
+        return idService;
+    }
+
+    public void setId(Long idService) {
+        this.idService = idService;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Prestataire>  getPrestataires() {
+        return prestataires;
+    }
+    public void setPrestataires(List<Prestataire>  prestataires) {
+        this.prestataires = prestataires;
+    }
+     
 }
---
+++ BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/ServiceRepository.java
@@ -0,0 +1,7 @@
+package com.groupe5.domaine;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, Integer>{
+    
+}
