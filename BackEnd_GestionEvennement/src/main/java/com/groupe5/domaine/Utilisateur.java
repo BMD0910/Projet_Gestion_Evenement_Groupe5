10184af929fb60b4074043a03b1a8154dff1600b
Mareme31 <m.t55@zig.univ.sn>
2023-11-11 11:40:57 +0000

    Mise a jour de la classe utilisateur et ajout  de son repository

--- BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/Utilisateur.java
+++ BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/Utilisateur.java
@@ -1,5 +1,112 @@
 package com.groupe5.domaine;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Entity;
+
+
+@Entity
 public class Utilisateur {
+
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable=false)
+	private int idUtilisateur;
     
+	@Column(nullable=false)
+	private String nom;
+     
+    @Column(nullable=false)
+    private String prenom ;
+	 
+    @Column(nullable=false)
+	private String password;
+
+	@Column(nullable=false)
+	private String role;
+    
+    @Column(nullable=false)
+    private String email;
+    
+/*Utilsation de OneToMany pour joindre les classes Utilisateur et Evenement */
+   @OneToMany(targetEntity = Evenement.class, mappedBy = "utilisateur", cascade = CascadeType.ALL)
+   private List<Evenement> evenements = new ArrayList<>();
+
+  /* --------------les Constructeurs--------------- */
+    public Utilisateur() {}
+
+    /*l'attribut id sera generer automatiquemnent donc il n'est pas necessaire de l'introduire dans la constructeur */
+    public Utilisateur(String nom, String prenom, String password, String role, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    /* --------------les getters et les setters--------------- */
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+       public List<Evenement> getEvenements() {
+    return evenements;
+    }
+
+    public void setEvenements(List<Evenement> evenements) {
+        this.evenements = evenements;
+    }
+  
 }
---
+++ BackEnd_GestionEvennement/src/main/java/com/groupe5/domaine/UtilisateurRepository.java
@@ -0,0 +1,5 @@
+package com.groupe5.domaine;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Integer>{}
